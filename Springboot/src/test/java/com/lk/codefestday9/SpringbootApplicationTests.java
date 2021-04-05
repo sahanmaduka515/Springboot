83c10512752a8e2aa77af76bdcd4217bd8ac3921
sahan maduka <savindadilshan0@gmail.com>
2021-04-05 21:01:04 +0530

    first commit

---
+++ Springboot/src/test/java/com/lk/codefestday9/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.codefestday9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
